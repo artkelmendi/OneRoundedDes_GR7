5c69652c127c4824bb693b53162c6fe6c4430bbf
Art Jashari <art.jashari1@student.uni-pr.edu>
2024-05-19 01:58:48 +0200

    Added A Welcome Panel
    - Usage instructions will be thrown here.
    - Attempted to debug the controller that handles the algorithm's logic.

--- src/main/java/Controllers/desroundController.java
+++ src/main/java/Controllers/desroundController.java
@@ -9,14 +9,20 @@ public class desroundController {
 
     @FXML
     private TextField leftHalfField;
+
+
     @FXML
     private TextField rightHalfField;
+
     @FXML
     private TextField subkeyField;
+
     @FXML
     private Text leftOutputText;
+
     @FXML
     private Text rightOutputText;
+
     @FXML
     private AnchorPane resultPane;
 
@@ -28,6 +34,10 @@ public class desroundController {
         String R = rightHalfField.getText();
         String K = subkeyField.getText();
 
+        System.out.println("L: " + L);
+        System.out.println("R: " + R);
+        System.out.println("K: " + K);
+
         try {
             if (L.length() != 32 || R.length() != 32 || K.length() != 48) {
                 throw new NumberFormatException("Invalid length");
@@ -37,7 +47,13 @@ public class desroundController {
             int rightHalf = Integer.parseUnsignedInt(R, 2);
             int subkey = Integer.parseUnsignedInt(K, 2);
 
+            System.out.println("Parsed leftHalf: " + leftHalf);
+            System.out.println("Parsed rightHalf: " + rightHalf);
+            System.out.println("Parsed subkey: " + subkey);
+
             int fOutput = fFunction(rightHalf, subkey);
+
+            System.out.println("fFunction output: " + fOutput);
 
             int newLeft = rightHalf;
             int newRight = leftHalf ^ fOutput;
@@ -45,10 +61,20 @@ public class desroundController {
             String newLeftStr = String.format("%32s", Integer.toBinaryString(newLeft)).replace(' ', '0');
             String newRightStr = String.format("%32s", Integer.toBinaryString(newRight)).replace(' ', '0');
 
+            System.out.println("newLeftStr: " + newLeftStr);
+            System.out.println("newRightStr: " + newRightStr);
+
+            // Update UI components
             leftOutputText.setText(newLeftStr);
             rightOutputText.setText(newRightStr);
 
+            System.out.println("leftOutputText set to: " + newLeftStr);
+            System.out.println("rightOutputText set to: " + newRightStr);
+
             resultPane.setVisible(true);
+
+            System.out.println("resultPane set to visible");
+
         } catch (NumberFormatException e) {
             handleInvalidInput();
         }
